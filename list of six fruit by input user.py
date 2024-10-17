b799800e2d12e0503e8230c5da3eff63e99d3607
mr.anand <madhurendraanand5@gmail.com>
2024-10-17 18:31:08 +0530

    Add files via upload

---
+++ list of six fruit by input user.py
@@ -0,0 +1,16 @@
+fruits = []
+
+f1 = input("enter your fruit name")
+fruits.append(f1)
+f2 = input("enter your fruit name")
+fruits.append(f2)
+f3 = input("enter your fruit name")
+fruits.append(f3)
+f4 = input("enter your fruit name")
+fruits.append(f4)
+f5 = input("enter your fruit name")
+fruits.append(f5)
+f6 = input("enter your fruit name")
+fruits.append(f6)
+f7 = input("enter your fruit name")
+fruits.append(f7)
